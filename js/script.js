8f6ee28ce86aafa86edc6cd2d70f1e1ee8c947e6
Sachin Mishra <aaryamishra421@gmail.com>
2024-05-20 12:41:29 +0530

    portfolioweb

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* ----- NAVIGATION BAR FUNCTION ----- */
+function myMenuFunction(){
+    var menuBtn = document.getElementById("myNavMenu");
+    if(menuBtn.className === "nav-menu"){
+      menuBtn.className += " responsive";
+    } else {
+      menuBtn.className = "nav-menu";
+    }
+  }
